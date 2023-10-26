1faab9a4373cde5cdf258e10a28855709b43cb44
Siddwin456 <siddhivinayakamech.rymec@gmail.com>
2023-10-26 12:53:48 +0530

    initial comit

---
+++ src/test/java/com/fastpix/devopsdemo/DevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastpix.devopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
